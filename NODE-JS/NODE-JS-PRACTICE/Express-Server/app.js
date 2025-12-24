a48db795e055b429ced5059caba5e8fdb7330b98
krish <pajiyawalakrish@gmail.com>
2025-12-24 16:13:15 +0530

    done

--- NODE-JS/NODE-JS-PRACTICE/Express-Server/app.js
+++ NODE-JS/NODE-JS-PRACTICE/Express-Server/app.js
@@ -4,6 +4,33 @@ const port = 8000;
 
 app.set('view engine', 'ejs');
 
+const students = [
+  {
+    id: 1,
+    name: "i",
+    age: 20,
+    email: "iam@test.com"
+  },
+  {
+    id: 2,
+    name: "he",
+    age: 30,
+    email: "he@test.com"
+  },
+  {
+    id: 3,
+    name: "she",
+    age: 40,
+    email: "she@test.com"
+  },
+  {
+    id: 4,
+    name: "it",
+    age: 50,
+    email: "it@test.com"
+  },
+]
+
 app.get('/', (req, res) => {
   res.end("Welcome to Express Server");
 }
